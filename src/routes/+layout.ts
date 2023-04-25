237434d81f7e96706ba7f2619ace038a69ed4bff
Wooseop Kim <wooseop@protonmail.com>
2023-04-26 06:02:59 +0900

    Add PostHog script

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,10 @@
-import { dev } from '$app/environment';
+import { browser, dev } from '$app/environment';
+import posthog from 'posthog-js';
+
+if (browser) {
+	posthog.init('phc_IaK96dVLTQYy3rsouvrEb9LcT59EpIkqmMhwK7dwouO', {
+		api_host: 'https://app.posthog.com',
+	});
+}
 
 export const prerender = !dev;
