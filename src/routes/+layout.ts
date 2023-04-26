c53e4da98defc67560bb88b09f76910073aed8d5
Wooseop Kim <wooseop@protonmail.com>
2023-04-27 03:15:18 +0900

    Disable PostHog on dev

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,7 @@
 import { browser, dev } from '$app/environment';
 import posthog from 'posthog-js';
 
-if (browser) {
+if (browser && !dev) {
 	posthog.init('phc_IaK96dVLTQYy3rsouvrEb9LcT59EpIkqmMhwK7dwouO', {
 		api_host: 'https://app.posthog.com',
 	});
